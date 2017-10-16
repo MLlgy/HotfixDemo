137dded7817dda8b378abc16e6fd12fb6c728bd6
liguoying <alinemklee@yeah.net>
2017-10-16 11:35:53 +0800

    second generate app

--- app/src/main/java/com/mk/hotfixdemo/MainActivity.kt
+++ app/src/main/java/com/mk/hotfixdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mk.hotfixdemo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -13,5 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tv_show_toast.setOnClickListener { Toast.makeText(this, msg, Toast.LENGTH_SHORT).show() }
+        tv_jump_two_second.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/mk/hotfixdemo/SecondActivity.kt
+++ app/src/main/java/com/mk/hotfixdemo/SecondActivity.kt
@@ -2,11 +2,15 @@ package com.mk.hotfixdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class SecondActivity : AppCompatActivity() {
-
+    var msg: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        msg = "secondActivity"
+        tv_jump_two_second.setOnClickListener { Toast.makeText(this, msg?.toString(), Toast.LENGTH_SHORT).show() }
     }
 }
