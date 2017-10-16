9fef037d43f418df9d798006e4f5f9943975fcca
liguoying <alinemklee@yeah.net>
2017-10-16 11:12:47 +0800

    first generge apk

--- app/src/main/java/com/mk/hotfixdemo/MainActivity.kt
+++ app/src/main/java/com/mk/hotfixdemo/MainActivity.kt
@@ -2,11 +2,16 @@ package com.mk.hotfixdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var msg: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_show_toast.setOnClickListener { Toast.makeText(this, msg, Toast.LENGTH_SHORT).show() }
     }
 }
